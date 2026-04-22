142caee933fe9ce75efa40d6f3b6413036cd8cb6
Deen <deenfransman@gmail.com>
2026-04-22 23:28:12 +0200

    added functions

--- structs.cpp
+++ structs.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <limits>
 
 using namespace std;
 
@@ -216,19 +217,155 @@ bool loginCustomer(Customer &loggedIn)
     return false;
 }
 
+void saveTransaction(const char accNum[], const char transType[], double amount){
+    Transaction t;
+
+    strcpy(t.accountNumber, accNum);
+    strcpy(t.type, transType);
+    t.amount = amount;
+
+    ofstream f("transactions.dat", ios::binary | ios::app);
+    if(!f){
+        cout << "Transaction file error!" << endl;
+        return;
+    }
+    f.write((char*)&t, sizeof(t));
+    f.close();
+}
+
+void viewTransactions(const Customer &c){
+    Transaction t;
+
+    bool found = false;
+
+    ifstream f("transactions.dat", ios::binary);
+
+    if(!f){
+        cout << "No transactions file found yet" << endl;
+        return;
+    }
+
+    cout << "\nTransaction history for " << c.name << endl;
+    cout << "Account Number: " << c.accountNumber << endl;
+
+    while(f.read((char*)&t, sizeof(t))){
+        if(strcmp(t.accountNumber, c.accountNumber) == 0){
+            cout << "Type: " << t.type << " | Amount: " << t.amount << endl;
+            found = true;
+        }
+    }
+
+    if(!found){
+        cout << "No transactions found for this account!" << endl;
+    }
+    f.close();
+}
+void deposit(Customer &c){
+    double amount;
+
+    cout << "Enter amount to deposit: ";
+    cin >> amount;
+
+    if(cin.fail() || amount <= 0){
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid amount!" << endl;
+        return;
+    }
+
+    c.balance += amount;
+
+    fstream f("customers.dat", ios::binary | ios::in | ios::out);
+
+    if(!f){
+        cout << "File Error!" << endl;
+        return;
+    }
+
+    Customer temp;
+
+    while (f.read((char*)&temp, sizeof(temp))){
+        if(strcmp(temp.accountNumber, c.accountNumber) == 0){
+            f.seekp(-sizeof(temp), ios::cur);
+            f.write((char*)&c, sizeof(c));
+            break;
+        }
+    }
+    f.close();
+    saveTransaction(c.accountNumber, "Deposit", amount);
+    cout << "Deposit Successful! New Balance: " << c.balance << endl;
+}
 
 
-void customerMenu(Customer c){
+void withdraw(Customer &c){
+    double amount;
+
+    cout << "Enter amount to withdraw: ";
+    cin >> amount;
+
+    if(cin.fail()){
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Please enter a valid number!" << endl;
+        return;
+    }
+
+    if(amount <= 0){
+        cout << "Amount must be greater than 0!" << endl;
+        return;
+    }
+
+    if(amount > c.balance){
+        cout << "Insufficient Funds!" << endl;
+        return;
+    }
+
+    c.balance -= amount;
+
+    fstream f("customers.dat", ios::binary | ios::in | ios::out);
+
+    if(!f){
+        cout << "File Error!" << endl;
+        return;
+    }
+
+    Customer temp;
+
+    while(f.read((char*)&temp, sizeof(temp))){
+        if(strcmp(temp.accountNumber, c.accountNumber) == 0){
+            f.seekp(-sizeof(temp), ios::cur);
+            f.write((char *)&c, sizeof(c));
+            break;
+        }
+    }
+    f.close();
+    saveTransaction(c.accountNumber, "Withdraw", amount);
+    cout << "Withdrawal Successful! New Balance: " << c.balance << endl;
+}
+
+
+
+
+void customerMenu(Customer &c){
     int choice;
 
     do{
-        cout << "\nCustomer Menu\n" << endl;
-        cout << "1. View Balance\n" << endl;
-        cout << "2. Deposit\n" << endl;
-        cout << "3. Withdraw\n" << endl;
-        cout << "4. Exit\n" << endl;
-        cout << "Enter choice: " << endl;
+        cout << "\nCustomer Menu\n";
+        cout << "1. View Balance\n";
+        cout << "2. Deposit\n";
+        cout << "3. Withdraw\n";
+        cout << "4. View Transactions\n";
+        cout << "5. Exit\n";
+        cout << "Enter choice: ";
         cin >> choice;
+
+        if(cin.fail()){
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Please enter number only...\n";
+            choice = 0;
+            continue;
+        }
 
         switch (choice)
         {
@@ -236,19 +373,25 @@ void customerMenu(Customer c){
             cout << "Balance: " << c.balance << endl;
             break;
         case 2:
-            cout << "Deposit coming next..." << endl;
+            deposit(c);
             break;
         case 3:
-            cout << "Withdraw coming next..." << endl;
+            withdraw(c);
             break;
         case 4:
+            viewTransactions(c);
+            break;
+        case 5:
             cout << "Exiting..." << endl;
+            break;
         default:
             cout << "Invalid Option" << endl;
             break;
         }
-    }while (choice != 4);
+    }while (choice != 5);
 }
+
+
 
 int main()
 {
